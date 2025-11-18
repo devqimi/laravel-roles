3d11d6d6a455864f0bd855925b9027b4990f7c92
Haqimi Imran <haqimiah05@gmail.com>
2025-11-18 09:48:42 +0800

    update border login/register

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -16,9 +16,10 @@ export default function AuthSimpleLayout({
 }: PropsWithChildren<AuthLayoutProps>) {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <div className="flex flex-col gap-8">
+            <div className="w-full max-w-md">
+                <div className="border border-gray-200 rounded-lg shadow-sm flex flex-col gap-8 p-6 shadow-md dark:border-gray-900">
                     <div className="flex flex-col items-center gap-4">
+                        
                         <Link
                             href={home()}
                             className="flex flex-col items-center gap-2 font-medium"
